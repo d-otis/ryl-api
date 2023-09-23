2110dafab50df26fd2e9277403f04d7c3ad66b5d
Dan Foley <d-otis@users.noreply.github.com>
2023-09-23 17:34:19 -0500

    boilerplate for landlords model/table/validations

--- db/migrate/20230923223019_create_landlords.rb
+++ db/migrate/20230923223019_create_landlords.rb
@@ -1,9 +1,7 @@
 class CreateLandlords < ActiveRecord::Migration[7.1]
   def change
     create_table :landlords, id: :uuid do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :company
+      t.string :name
 
       t.timestamps
     end
---
+++ spec/models/landlord_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+include FactoryBot::Syntax::Methods
+
+RSpec.describe Landlord, type: :model do
+  describe "validations" do
+    context "when name is present" do
+      it "is valid" do
+        expect(build(:landlord)).to be_valid
+      end
+    end
+
+    context "when name is not present" do
+      it "is invalid" do
+        expect(build(:landlord, name: nil)).to be_invalid
+      end
+    end
+  end
+end
