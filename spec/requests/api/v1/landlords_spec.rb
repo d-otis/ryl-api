8ec539567602b26a2e3c45c6feb1d54659f98a8f
Dan Foley <d-otis@users.noreply.github.com>
2023-09-24 22:15:46 -0500

    indentation

--- spec/requests/api/v1/landlords_spec.rb
+++ spec/requests/api/v1/landlords_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::LandlordsController, type: :controller do
-    describe "GET /show" do
-      context "when the landlord exists" do
-        before(:each) do
-          allow(Landlord).to receive(:find).and_return(FactoryBot.build_stubbed(:landlord))
-        end
-
-        it "returns http success" do
-          get :show, params: { id: 'a-uuid' }
-          expect(response).to have_http_status(:success)
-        end
-      
-        it "assigns @landlord" do
-          get :show, params: { id: 'a-uuid' }
-          expect(assigns(:landlord)).to eq(Landlord.find('a-uuid'))
-        end
+  describe "GET /show" do
+    context "when the landlord exists" do
+      before(:each) do
+        allow(Landlord).to receive(:find).and_return(FactoryBot.build_stubbed(:landlord))
       end
 
-      context "when the landlord does not exist" do
-        before(:each) do
-          allow(Landlord).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
-        end
-
-        it "returns http not found" do
-          get :show, params: { id: 'a-uuid' }
-          expect(response).to have_http_status(:not_found)
-        end
+      it "returns http success" do
+        get :show, params: { id: 'a-uuid' }
+        expect(response).to have_http_status(:success)
+      end
+    
+      it "assigns @landlord" do
+        get :show, params: { id: 'a-uuid' }
+        expect(assigns(:landlord)).to eq(Landlord.find('a-uuid'))
       end
     end
+
+    context "when the landlord does not exist" do
+      before(:each) do
+        allow(Landlord).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
+      end
+
+      it "returns http not found" do
+        get :show, params: { id: 'a-uuid' }
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
 
   describe "GET /index" do
     it "returns http success" do
