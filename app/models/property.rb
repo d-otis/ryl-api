f4d5f9da7b32f22f5e3dd8e94fe2fa6208051d16
Dan Foley <d-otis@users.noreply.github.com>
2023-10-02 23:11:40 -0500

    first pass @ property model and tests

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+  validates_presence_of :address
+  after_initialize :set_default_rating
+
+  def set_default_rating
+    self.rating = 0
+  end
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when required attributes are present" do
+    it "is valid" do
+      expect(FactoryBot.build(:property)).to be_valid
+    end
+  end
+
+  context "when required attributes are missing" do
+    it "is invalid w/o address" do
+      expect(FactoryBot.build(:property, address: nil)).to be_invalid
+    end
+  end
+
+  describe "after_initialize_method" do
+    it "initializes the rating @ 0.0" do
+      property = Property.new(address: "123 South St")
+      expect(property.rating).to eq(0.0)
+      expect(property).to be_valid
+    end
+  end
 end
